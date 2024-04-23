7358dc60ed981aead40606ee39da7c3c91c4a62b
FlyaginA <67369302+FlyaginA@users.noreply.github.com>
2024-04-23 16:45:33 +0200

    Update
    return of the mobile functionality

---
+++ ATlearning/ATframework3demo/TestCases/Web/NewsPage_Cases/Case_Bitrix24_NewsPage_AttachingAFile.cs
@@ -0,0 +1,118 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.PageObjects;
+using atFrameWork2.SeleniumFramework;
+using ATframework3demo.PageObjects;
+using ATframework3demo.PageObjects;
+using ATframework3demo.PageObjects.Drive;
+
+namespace ATframework3demo.TestCases.Web.NewsPage_Cases
+{
+    public class Case_Bitrix24_NewsPage_AttachingAFile : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("Прикрепление файла к сообщению в новостной ленте (>5Mb)", homePage => AttachAFile(homePage)));
+            return caseCollection;
+        }
+
+        void AttachAFile(PortalHomePage homePage)
+        {
+            //укажем название и путь загружаемого файла
+            string filename = "how developers think.MP4";
+            string filepath = "D:\\bitrix\\lab2(auto test)\\ConsoleApp1\\how developers think.MP4";
+
+            //сначала стоит проверить наличие файла, аналогичного загружаемому
+            if (new NewsPage().PrerequisiteCaseBitrix24NewsPageAttachingFile(filename) == false)
+            {
+
+                homePage
+                    .NewsPage
+                    //находим пост с таким файлом
+                    .FindNewsByString(filename)
+                    //открываем меню поста
+                    .OpenPostMenu()
+                    //удаляем пост с таким файлом
+
+                    .Delete();
+                //удаляем из диска
+            }
+            homePage
+                //проверяем отсутствие загружаемого файла на диске 
+                //открываем левое меню
+                .LeftMenu
+                //переходим в диск
+                .Drive()
+                //выбираем папку "загруженные файлы"
+                .DownloadFiles()
+                //открываем эту папку
+                .OpenMenu()
+                .Open();
+
+            if (new MyDrive().PrerequisiteCaseBitrix24NewsPageAttachingFile(filename) == true)
+            {
+                homePage
+                    .MyDrive
+                //находим файл с названием файла
+                    .FindElementByString(filename)
+                    //открываем меню
+                    .OpenMenu()
+                    //удалить файл
+                    .Delete()
+                    //подтверждение
+                    .Accept();
+                //обновление страницы
+                WebDriverActions.Refresh();
+                //возвращаемся на главную страницу
+                homePage.LeftMenu.OpenNews();
+            }
+            //возвращаемся на главную страницу
+            homePage
+                .LeftMenu
+                .OpenNews();
+            //Появляемся на главной странице портала
+            homePage
+                .NewsPage
+                //открыть написание нового поста
+                .AddPost()
+                //включить в панели инструментов прикрепление файла
+                .Toolbar_NewPost
+                .AttachFile()
+                //прикрепить файл
+                .fileInput
+                .UploadFile(filepath);
+
+
+            //проверить что прикрепленный файл существует
+            homePage
+                .NewsPage
+                .PostForm
+                .Assert_AttachedFiles()
+
+                //Опубликовать пост
+                .SendPost();
+            //Обновить страницу, проверить наличие загруженного файла на странице
+            WebDriverActions.Refresh();
+            //проверить наличие поста с прикрепленным файлом
+            homePage
+                .NewsPage
+                .Assert_Case_Bitrix24_NewsPage_AttachingFile(filename);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ATlearning/ATframework3demo/TestCases/Web/WorkGroups/Case_Bitrix_CreateProject+CreatePost.cs
@@ -0,0 +1,78 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.BaseFramework.LogTools;
+using atFrameWork2.PageObjects;
+using atFrameWork2.SeleniumFramework;
+using ATframework3demo.PageObjects;
+using ATframework3demo.PageObjects.WorkGroups;
+using ATframework3demo.TestEntities;
+
+
+namespace ATframework3demo.TestCases.Web.WorkGroups
+{
+    public class Case_Bitrix_CreateProject_CreatePost : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("создание проекта + пост в ленту проекта", homePage => CreateProjectCreatePost(homePage)));
+            return caseCollection;
+        }
+
+        void CreateProjectCreatePost(PortalHomePage homePage)
+        {
+            //задаем название нашему проекту и создадим его сущность
+            TestWorkgroup workgroup = new TestWorkgroup("Test" + DateTime.Now.Ticks);
+            //Задаем текст сообщения, которое будем отправлять на странице проекта и создадим его сущность
+            TestPost post = new TestPost("TestPost" + DateTime.Now.Ticks);
+            bool WorkGroupWasCreated = 
+            homePage
+                //Перейти в раздел "группы"
+                .LeftMenu
+                .WorkGroups()
+            //Создать новую сущность
+                .ProjectAddButton()
+            //Выбрать "проект", следующий шаг
+                .ChooseProject()
+                .NextStep()
+            //Вписать название, следующий шаг
+                .EnterNameProject(workgroup.WorkGroupName)
+                .NextStep()
+            //выбрать открытый, следующий шаг
+                .ChooseOpen()
+                .NextStep()
+            //не выбирать участников, перейти на следующий шаг
+                .NextStep()
+            //выбрать окно написания поста
+                .AddPost()
+            //написать текст тестового сообщения
+                .EnterText(post.PostText)
+            //отправить сообщение
+                .SendPost()
+            //Закрыть Слайдер
+                .CloseSlider()
+            //Проверить наличие проекта с нашим названием
+                .Search(workgroup.WorkGroupName)
+                .ChooseWorkGroupItem(workgroup.XPath)
+                .WaitElementDisplayed();
+            if (!WorkGroupWasCreated)
+            {
+                Log.Error("Проект с заданным названием не отображается");
+            }
+            //проверить наличие созданного поста
+            bool ProjectPostExists =
+                new WorkGroupsPage()
+                    //выберем в списке элемент соответстующий нашему названию
+                    .ChooseWorkGroupItem(workgroup.XPath)
+                    //открыть проект с нашим названием
+                    .OpenWorkGroup()
+                    //проверить наличие созданного поста
+                    .Search(post.PostText)
+                    .PostByTextExist(post.PostText);
+
+            if (!ProjectPostExists)
+            {
+                Log.Error("Пост с заданным текстом не отображается");
+            }
+        }
+    }
+}
